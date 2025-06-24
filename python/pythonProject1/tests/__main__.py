5cadd216baf4deb0f890a069d71830f5020d89cb
lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2025-06-23 20:13:58 -0400

    Colored test suite main file.

--- python/pythonProject1/tests/__main__.py
+++ python/pythonProject1/tests/__main__.py
@@ -1,5 +1,9 @@
 import test_qafny_grammar as tqg
 import test_program_transformer as tpt
 
+import rich
+
+rich.print('[magenta]Running ANTLR Grammar Tests:')
 tqg.run()
+rich.print('[magenta]Running ProgramTransformer Tests:')
 tpt.run()
