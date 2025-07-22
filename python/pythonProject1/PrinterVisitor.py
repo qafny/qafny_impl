b2cb5e7c6138638c53379273e77a143c69a6aba4
liyili2 <liyili2@illinois.edu>
2025-07-22 09:21:49 -0500

    update.

--- python/pythonProject1/PrinterVisitor.py
+++ python/pythonProject1/PrinterVisitor.py
@@ -155,10 +155,7 @@ class PrinterVisitor(TargetProgramVisitor):
 
         self.current_line += 1
         return 'if (' + ctx.cond().accept(self) + '){\n' + stmts + '}' + elsepart
-    
-    def visitVar(self, ctx: TargetProgrammer.DXVar):
-        return ctx.ID()
-    
+
     def visitIndex(self, ctx: TargetProgrammer.DXIndex):
         return ctx.bind().accept(self) + '[' + ctx.index().accept(self) + ']'
     
