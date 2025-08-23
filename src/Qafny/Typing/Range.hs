136068d254b2bddc3c458a8196b16cf1a93ebe31
lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2025-08-23 16:09:54 -0500

    Removed all old Haskell files

--- src/Qafny/Typing/Range.hs
+++
@@ -1,7 +0,0 @@
-module Qafny.Typing.Range where
-import Qafny.Syntax.AST (Range)
-
--- Utility functions for range related behaviors
-
-areRangesEquiv :: [(Range, Range)] -> Bool
-areRangesEquiv = all (uncurry (==))
